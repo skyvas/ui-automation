37736fbf02512822dea061353cc8c56d33695a49
AkashDeepVashista <akash.vashista@platotech.com>
2025-01-09 13:58:04 -0800

    test passing test

--- src/test/java/pageobjects/SummaryPage.java
+++ src/test/java/pageobjects/SummaryPage.java
@@ -12,7 +12,7 @@ public class SummaryPage {
     }
 
     // Locator for the total label div
-    private By totalLabel = By.cssSelector("div.summary_total_label[data-test='total-labe']");
+    private By totalLabel = By.cssSelector("div.summary_total_label[data-test='total-label']");
 
     // Method to retrieve the total value as a string (without the "Total: $" part)
     public String getTotalValue() {
